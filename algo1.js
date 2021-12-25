ae37d32f9d58980903025149e859e24e7a6cb73b
Ryu <ry5tq@virginia.edu>
2021-12-25 14:47:11 -0500

    adding algo files

---
+++ algo1.js
@@ -0,0 +1,23 @@
+let row = 5;
+let col = 5;
+let m = [];
+
+let startCol = 0;
+let startRow = 0;
+
+let nextCol;
+let nextRow;
+
+//counter variables for steps taken
+let totalMoves = 0;
+let nodesLeft = 0;
+let nodesNext = 1;KeyboardEvent
+
+//end  reached?
+let reachedEnd = false;
+
+//matrix to keep track of visited cells
+let visited = [];
+
+//vectors for direction when determining neighbors
+
