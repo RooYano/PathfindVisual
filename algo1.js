307df4c3a7f38c95057adacacbab57ac254b5d51
Ryu <ry5tq@virginia.edu>
2021-12-25 15:31:09 -0500

    For queueing new nodes, linkedlist implemented

--- algo1.js
+++ algo1.js
@@ -1,23 +1,70 @@
 let row = 5;
 let col = 5;
-let m = [];
+let m = []; //nested array for matrix to create a grid of size row x col
 
-let startCol = 0;
-let startRow = 0;
+class Node {
+    constructor(value){
+        this.val = value;
+        this.next = null;
+    }
+}
 
-let nextCol;
-let nextRow;
+class Queue {
+    constructor(head = null) {
+        this.head = head;
+    }
+}
+
+let startCol = new Node(0);
+let startRow = new Node(0);
+
+Queue.prototype.enqueue = function (data){
+    let nodeAdd = new Node(data);
+
+    //if list is empty, the head will point to new node
+    if(!this.head){
+        this.head = nodeAdd;
+        return this.head;
+    }
+
+
+    //i can optimize this later
+    let tail = this.head;
+    while (tail.next !== null){
+        tail = tail.next;
+    }
+    tail.next = nodeAdd;
+
+    return this.head
+}
+
+/*test code
+
+startCol;
+nextCol.enqueue(3);
+nextCol.enqueue(4);
+
+*/
+
+//queue
+let nextCol = new Queue(startCol);
+let nextRow = new Queue(startRow); 
 
 //counter variables for steps taken
 let totalMoves = 0;
-let nodesLeft = 0;
-let nodesNext = 1;KeyboardEvent
+let nodesLeft = 0; //adding nodes to check like peels of an onion. shows how many to dequeue before moving to next layer
+let nodesNext = 1; //
 
 //end  reached?
 let reachedEnd = false;
 
-//matrix to keep track of visited cells
+//boolean matrix to keep track of visited cells
 let visited = [];
 
 //vectors for direction when determining neighbors
+deltaRow = [-1, 1, 0, 0];
+deltaCol = [0, 0, 1, -1];
 
+function findPath(){
+
+}
